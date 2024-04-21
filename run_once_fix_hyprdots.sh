41f7d67ca6524f490bdfc39c4329649ac439403e
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-04-21 03:07:51 -0300

    update

--- run_once_fix_hyprdots.sh
+++ run_once_fix_hyprdots.sh
@@ -4,11 +4,16 @@ fix_catppuccin_bar_bg() {
   echo "@define-color bar-bg #11111b;" >> ~/.config/waybar/theme.css
 }
 
+fix_change_shell () {
+  sudo chsh --shell /bin/bash mrcapivaro
+}
+
 OS="$(uname -s)"
 case "${OS}" in
 Linux*)
   if [ -f /etc/arch-release ]; then
     fix_catppuccin_bar_bg
+    fix_change_shell
   fi
   ;;
 *)
