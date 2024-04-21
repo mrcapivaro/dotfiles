d84e3bb3f642be5dab30f9d2a26acefb910940f5
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-04-21 03:30:33 -0300

    update

--- run_once_fix_hyprdots.sh
+++ run_once_fix_hyprdots.sh
@@ -8,12 +8,18 @@ fix_change_shell () {
   sudo chsh --shell /bin/bash mrcapivaro
 }
 
+fix_xremap_sudo () {
+  sudo gpasswd -a mrcapivaro input
+  echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
+}
+
 OS="$(uname -s)"
 case "${OS}" in
 Linux*)
   if [ -f /etc/arch-release ]; then
     fix_catppuccin_bar_bg
     fix_change_shell
+    fix_xremap_sudo
   fi
   ;;
 *)
