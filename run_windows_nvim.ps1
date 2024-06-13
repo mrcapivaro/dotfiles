72fc6cc89b8ca29a63abc914614aaeaf5948d6ef
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-06-13 18:04:06 -0300

    update

---
+++ run_windows_nvim.ps1
@@ -0,0 +1,21 @@
+$targetDir = "$env:USERPROFILE\.local\share\chezmoi\dot_config\nvim"
+$linkDir = "$env:LOCALAPPDATA\nvim"
+
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -Wait -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+
+if (-Not (Test-Path $linkDir)) {
+  try {
+    New-Item -ItemType SymbolicLink -Target $targetDir -Path $linkDir
+    Write-Host "Symlink of dotfiles/chezmoi nvim to local appdata created."
+  } catch {
+    Write-Host "Failed to create nvim symlink."
+  }
+}
+# asdlkfjsajdkf
