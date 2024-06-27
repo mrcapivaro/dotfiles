c22639800f391785bda4d58aec52681aff63ef8a
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-06-26 21:48:02 -0300

    update

--- AppData/Roaming/wezterm/wezterm.lua
+++ AppData/Roaming/wezterm/wezterm.lua
@@ -4,11 +4,11 @@ local wezterm = require("wezterm")
 M.color_scheme = "Catppuccin Mocha"
 M.window_close_confirmation = "NeverPrompt"
 M.font = wezterm.font({
-  family = "FantasqueSansM Nerd Font",
+  family = "CaskaydiaMono Nerd Font Mono",
   weight = 400,
 })
 M.font_size = 14
--- M.window_background_opacity = 0.95
+M.window_background_opacity = 0.95
 M.use_fancy_tab_bar = false
 M.tab_bar_at_bottom = false
 M.window_decorations = "INTEGRATED_BUTTONS" .. "|" .. "RESIZE"
