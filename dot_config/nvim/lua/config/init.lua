588b10c9fdb4397de798075a552464826462b379
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-06-28 21:03:16 -0300

    update

--- dot_config/nvim/lua/config/init.lua
+++ dot_config/nvim/lua/config/init.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.start = function()
   vim.cmd([[colorscheme ]] .. M.base_colorscheme)
-  vim.cmd([[language en_US.UTF-8]])
+  -- vim.cmd([[language en_US.UTF-8]])
   require("config.keymaps")
   require("config.options")
   if M.plugins then
