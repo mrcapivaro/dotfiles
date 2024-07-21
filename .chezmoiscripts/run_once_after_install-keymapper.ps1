b9543cc027fdfb2595d92ec4ebd378ea1e338566
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-07-21 14:58:11 -0300

    update

--- .chezmoiscripts/run_once_after_install-keymapper.ps1
+++ .chezmoiscripts/run_once_after_install-keymapper.ps1
@@ -2,15 +2,25 @@ $temp = "$HOME\\Downloads\\keymapper"
 $dest = "$HOME\\scoop\\shims\\"
 $url = "https://github.com/houmain/keymapper/releases/download/4.4.5/keymapper-4.4.5-win64.msi"
 
-if (-Not (Test-Path -Path "$HOME\\Downloads\\keymapper\\")) {
-  New-Item -ItemType Directory -Path $temp -Force
-  Invoke-WebRequest -Uri $url -OutFile "$temp\\keymapper.zip"
-}
+if (-Not (Test-Path -Path "$dest\\keymapper.exe")) {
+  if (-Not (Test-Path -Path "$HOME\\Downloads\\keymapper\\")) {
+    New-Item -ItemType Directory -Path $temp -Force
+    Invoke-WebRequest -Uri $url -OutFile "$temp\\keymapper.zip"
+    7z x "$temp\\keymapper.zip" -o"$dest" -y
+  }
 
-if (-Not (Test-Path -Path "$dest\\scoop\\shims\\CM_FP_keymapper.exe")) {
-  7z x $temp\\keymapper.zip -o"$dest" -y
-}
+  if (Test-Path -Path "$dest\\CM_FP_keymapper*.exe") {
+    Move-Item -Path "$dest\\CM_FP_keymapper.exe" -Destination "$dest\\keymapper.exe"
+    Move-Item -Path "$dest\\CM_FP_keymapperd.exe" -Destination "$dest\\keymapperd.exe"
+    Move-Item -Path "$dest\\CM_FP_keymapperctl.exe" -Destination "$dest\\keymapperctl.exe"
+  }
 
-if (Test-Path -Path "$HOME\\Downloads\\keymapper\\") {
-  Remove-Item -Path $temp -Recurse -Force
+  if (Test-Path -Path "$HOME\\Downloads\\keymapper\\") {
+    Remove-Item -Path $temp -Recurse -Force
+  }
+
+  sudo "$dest\keymapperd.exe"
+  sudo "$dest\keymapper.exe"
+  sudo schtasks /create /tn RunKeymapperd /tr "$dest\keymapperd.exe" /sc onstart /f
+  sudo schtasks /create /tn RunKeymapper /tr "$dest\keymapper.exe" /sc onstart /f
 }
--- AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/keymapper.vbs
+++ AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/keymapper.vbs
@@ -1,10 +1,10 @@
-Set WshShell = CreateObject("WScript.Shell")
+Set Shell = CreateObject("WScript.Shell")
 
-homeDir = WshShell.ExpandEnvironmentStrings("%USERPROFILE%")
+homeDir = Shell.ExpandEnvironmentStrings("%USERPROFILE%")
 keymapper = homeDir & "\scoop\shims\keymapper.exe"
 keymapperd = homeDir & "\scoop\shims\keymapperd.exe"
 
-WshShell.Run keymapperd, 0, True
-WshShell.Run keymapper, 0, True
+Shell.Run keymapperd, 0, True
+Shell.Run keymapper, 0, True
 
-Set WshShell = Nothing
+Set Shell = Nothing
