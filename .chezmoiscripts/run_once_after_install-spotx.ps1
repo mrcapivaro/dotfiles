b94c76adaf5fcfa43b61a899d6b60d90345ba41d
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-07-22 01:19:48 -0300

    update

---
+++ .chezmoiscripts/run_once_after_install-spotx.ps1
@@ -0,0 +1,3 @@
+# spotx on github:
+# https://github.com/SpotX-Official/SpotX/tree/main
+iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -new_theme"
