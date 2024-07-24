627ee69cb910ba2c1ad09f7efb6b8271345f89d2
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-07-24 00:15:03 -0300

    update

--- Documents/Powershell/profile.ps1
+++ Documents/Powershell/profile.ps1
@@ -1,20 +1,22 @@
 Invoke-Expression (&starship init powershell)
-
 Import-Module Z
-
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-function which($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue |
-      Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
 Set-Alias ll ls
 Set-Alias vi nvim
 Set-Alias vim nvim
+Set-Alias clera clear
+Set-Alias cler clear
+Set-Alias clar clear
+Set-Alias c clear
 
 function chezmoicd() {
   Set-Location -Path $(chezmoi source-path)
+}
+
+function which($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+      Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
