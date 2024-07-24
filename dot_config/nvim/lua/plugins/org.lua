5d1f81781da60a5fcbfd75227c25c443d1f88a48
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-07-24 19:07:27 -0300

    update

--- dot_config/nvim/lua/plugins/org.lua
+++ dot_config/nvim/lua/plugins/org.lua
@@ -53,44 +53,51 @@ return {
     },
   },
 
+  -- does not work on windows:
+  -- kitty is not supported for windows
+  -- and ueberzugcc also
   -- {
   --   "3rd/image.nvim",
   --   opts = {},
   -- },
 
-  -- {
-  --   "nvim-neorg/neorg",
-  --   lazy = false,
-  --   version = "*",
-  --   config = function()
-  --     require("neorg").setup({
-  --       -- https://github.com/nvim-neorg/neorg/wiki
-  --       load = {
-  --         ["core.defaults"] = {},
-  --         ["core.concealer"] = {},
-  --         ["core.dirman"] = {
-  --           config = {
-  --             workspaces = { org = "~/org" },
-  --             default_workspace = "org",
-  --             index = "index.norg",
-  --           },
-  --         },
-  --         ["core.completion"] = {
-  --           engine = "cmp-nvim",
-  --         },
-  --         ["core.export"] = {},
-  --         ["core.export.markdown"] = {}, -- needed?
-  --         ["core.latex.renderer"] = {},
-  --         ["core.presenter"] = {
-  --           zen_mode = "truezen",
-  --         },
-  --         ["core.summary"] = {},
-  --         ["core.text-objects"] = {},
-  --       },
-  --     })
-
-  --     vim.wo.foldlevel = 99
-  --     vim.wo.conceallevel = 2
-  --   end,
-  -- },
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    version = "*",
+    config = function()
+      require("neorg").setup({
+        -- https://github.com/nvim-neorg/neorg/wiki
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = { org = "~/org" },
+              default_workspace = "org",
+              index = "index.norg",
+            },
+          },
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          },
+          ["core.export"] = {},
+          ["core.export.markdown"] = {},
+          -- does not work on windows: needs image.nvim
+          -- ["core.latex.renderer"] = {},
+          ["core.presenter"] = {
+            config = {
+              zen_mode = "truezen",
+            },
+          },
+          ["core.summary"] = {},
+          ["core.text-objects"] = {},
+        },
+      })
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
 }
