e63f950373f88a35ef1ebca8818fa67779ae07af
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-08-08 17:38:07 -0300

    update

--- dot_config/Powershell/profile.ps1
+++ dot_config/Powershell/profile.ps1
@@ -1,4 +1,16 @@
 Invoke-Expression (&starship init powershell)
+# Hook OSC 7 and Starship to allow the detection of the cwd in wezterm
+$prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
+}
+
 Import-Module Z
 # Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
@@ -6,14 +18,10 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 
 Set-Alias vi nvim
 Set-Alias vim nvim
+Set-Alias claer clear
 Set-Alias clera clear
 Set-Alias cler clear
 Set-Alias clar clear
-Set-Alias c clear
-
-function chezmoicd() {
-  Set-Location -Path $(chezmoi source-path)
-}
 
 function which($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
