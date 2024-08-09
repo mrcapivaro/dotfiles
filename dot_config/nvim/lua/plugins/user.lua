a67192ffff252155ec467797561248bab82612d0
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-08-09 13:56:58 -0300

    update

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -1,10 +1,5 @@
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
-
 ---@type LazySpec
 return {
-	-- { "windwp/nvim-ts-autotag", enabled = false },
-
 	{
 		"max397574/better-escape.nvim",
 		enabled = true,
@@ -39,39 +34,6 @@ return {
 			},
 		},
 	},
-
-	-- == Examples of Adding Plugins ==
-
-	-- "andweeb/presence.nvim",
-	-- {
-	--   "ray-x/lsp_signature.nvim",
-	--   event = "BufRead",
-	--   config = function() require("lsp_signature").setup() end,
-	-- },
-
-	-- == Examples of Overriding Plugins ==
-
-	-- customize alpha options
-	-- {
-	--   "goolord/alpha-nvim",
-	--   opts = function(_, opts)
-	--     -- customize the dashboard header
-	--     opts.section.header.val = {
-	--       " █████  ███████ ████████ ██████   ██████",
-	--       "██   ██ ██         ██    ██   ██ ██    ██",
-	--       "███████ ███████    ██    ██████  ██    ██",
-	--       "██   ██      ██    ██    ██   ██ ██    ██",
-	--       "██   ██ ███████    ██    ██   ██  ██████",
-	--       " ",
-	--       "    ███    ██ ██    ██ ██ ███    ███",
-	--       "    ████   ██ ██    ██ ██ ████  ████",
-	--       "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-	--       "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-	--       "    ██   ████   ████   ██ ██      ██",
-	--     }
-	--     return opts
-	--   end,
-	-- },
 
 	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
 	-- {
