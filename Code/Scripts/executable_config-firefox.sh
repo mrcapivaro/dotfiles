cbc3318ce0c4f0b8845bc7048c71e8b4a8d5da3a
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-08-31 20:30:31 -0300

    Update Code/Scripts/config-firefox.sh

--- Code/Scripts/executable_config-firefox.sh
+++ Code/Scripts/executable_config-firefox.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Path of the default-release profile
-PROFILE_PATH="$(ls ~/.mozilla/firefox/ | awk '/.*default-release/')"
-CONF_FILES_PATH="~/.local/share/chezmoi/.other/firefox/"
+PROFILE_PATH="$HOME/.mozilla/firefox/$(ls ~/.mozilla/firefox/ | awk '/.*default-release/')"
+CONF_FILES_PATH="$HOME/.local/share/chezmoi/.other/firefox/"
 
 echo "Copying config files for firefox from dotfiles repo to current default-release profile."
 cp "$CONF_FILES_PATH"/* "$PROFILE_PATH"
