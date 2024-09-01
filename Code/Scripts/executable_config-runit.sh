932f7b22c5f55bcf6cf26634ec454db56233af6a
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-01 03:49:52 -0300

    Update Code/Scripts/config-runit.sh

--- Code/Scripts/executable_config-runit.sh
+++ Code/Scripts/executable_config-runit.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
 
 # Keymapper setup
-sudo cp -r "$HOME/.config/sv/keymapper" /etc/runit/sv/
+KEYMAPPER_SV_DIR="/etc/runit/sv/keymapper"
+if [ -d "$KEYMAPPER_SV_DIR" ]; then
+  echo "The keymapper sv folder already exists at $KEYMAPPER_SV_DIR."
+else
+  echo "Copying keymapper sv files from .config to $KEYMAPPER_SV_DIR."
+  sudo cp -r "$HOME/.config/sv/keymapper" /etc/runit/sv/
+fi
+echo "Symlinking the sv dir to runit."
 sudo ln -s /etc/runit/sv/keymapper /run/runit/service/
