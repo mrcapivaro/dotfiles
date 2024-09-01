184f68ecd5d1aecdbe9af3bb31f9fc3d582eba76
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-01 04:00:27 -0300

    Update Code/Scripts/config-runit.sh

--- Code/Scripts/executable_config-runit.sh
+++ Code/Scripts/executable_config-runit.sh
@@ -4,7 +4,7 @@ set -e
 # Keymapper setup
 KEYMAPPER_CONF_DIR="$HOME/.config/sv/keymapperd"
 KEYMAPPER_SV_DIR="/etc/runit/sv/keymapperd"
-KEYMAPPER_TARGET_DIR=
+KEYMAPPER_TARGET_DIR="/run/runit/service/keymapperd"
 if [ -d "$KEYMAPPER_SV_DIR" ]; then
   echo "The keymapper sv folder already exists at $KEYMAPPER_SV_DIR."
 else
