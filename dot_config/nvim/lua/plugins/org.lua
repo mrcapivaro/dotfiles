a8ca16b6a7890f2ce6541c65f7880a21b3500a25
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-01 01:25:18 -0300

    update

--- dot_config/nvim/lua/plugins/org.lua
+++ dot_config/nvim/lua/plugins/org.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "nvim-neorg/neorg",
-    lazy = false,
-    version = "*",
-    opts = {
-      load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {},
-      },
-    },
-  },
+  -- {
+  --   "nvim-neorg/neorg",
+  --   lazy = false,
+  --   version = "*",
+  --   opts = {
+  --     load = {
+  --       ["core.defaults"] = {},
+  --       ["core.concealer"] = {},
+  --     },
+  --   },
+  -- },
 
-  {
-    "3rd/image.nvim",
-    enabled = function()
-      return vim.fn.has("win32") ~= 1
-    end,
-    config = true,
-  },
+  -- {
+  --   "3rd/image.nvim",
+  --   enabled = function()
+  --     return vim.fn.has("win32") ~= 1
+  --   end,
+  --   config = true,
+  -- },
 }
