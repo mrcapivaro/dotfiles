c80cf9be981cdf8ac3ba7105c63b2649ed3491fa
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-01 13:18:34 -0300

    update

--- dot_config/nvim/lua/statusline.lua
+++ dot_config/nvim/lua/statusline.lua
@@ -113,9 +113,9 @@ function M.gitsigns()
     return ""
   end
   local changes = {
-    (status.added ~= 0) and ("+" .. status.added) or "",
-    (status.removed ~= 0) and ("-" .. status.removed) or "",
-    (status.changed ~= 0) and ("~" .. status.changed) or "",
+    status.added and (status.added ~= 0) and ("+" .. status.added) or "",
+    status.removed and (status.removed ~= 0) and ("-" .. status.removed) or "",
+    status.changed and (status.changed ~= 0) and ("~" .. status.changed) or "",
   }
   return table.concat({
     " ",
