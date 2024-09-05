6f4a96d7a0cafe8de7a4ca8042536fa5453af0f0
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-05 01:57:37 -0300

    update

--- Code/Scripts/executable_config-firefox.sh
+++ Code/Scripts/executable_config-firefox.sh
@@ -4,14 +4,14 @@ set -e
 CONF_FILES_PATH="$HOME/.config/firefox"
 PROFILE_PATH="$HOME/.mozilla/firefox/$(ls $HOME/.mozilla/firefox/ | awk '/default-release/')"
 
-if test "$CONF_FILES_PATH/user.js"; then
+if [ -e "$CONF_FILES_PATH/user.js" ]; then
 	echo "The user.js file already exists inside the default-release profile folder."
 else
 	echo "Creating the user.js symlink."
 	ln -sf "$CONF_FILES_PATH/user.js" "$PROFILE_PATH/user.js"
 fi
 
-if test "$CONF_FILES_PATH/chrome"; then
+if [ -d "$CONF_FILES_PATH/chrome" ]; then
 	echo "The chrome folder already exists inside the default-release profile folder."
 else
 	echo "Creating the chrome/ symlink."
