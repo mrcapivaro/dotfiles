78a023c2df0cc4e373c4039e9b7ab51e2f89b6c0
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-05 18:53:43 -0300

    Update Code/Scripts/config-firefox.ps1

--- Code/Scripts/config-firefox.ps1
+++ Code/Scripts/config-firefox.ps1
@@ -1,5 +1,5 @@
 $profilePath = (Get-ChildItem -Path "$HOME\AppData\Roaming\Mozilla\Firefox\Profiles\" | Where-Object { $_.Name -match "default-release" }).toString()
-$profilePath
+$scoopProfPath = "$HOME\scoop\persist\firefox\profile"
 
 if ($profilePath)
 {
@@ -19,4 +19,24 @@ if ($profilePath)
   {
     Write-Host "There is no user.js file in .config/firefox"
   }
+} elseif (Test-Path -Path $scoopProfPath)
+{
+  $userjsPath = "$HOME\.config\firefox\user.js"
+  $chromePath = "$HOME\.config\firefox\chrome"
+  if (Test-Path -Path $chromePath)
+  {
+    New-Item -ItemType SymbolicLink -Path "$scoopProfPath\chrome" -Value $chromePath -Force
+  } else
+  {
+    Write-Host "There is no chrome/ folder in .config/firefox"
+  }
+  if (Test-Path -Path $userjsPath)
+  {
+    New-Item -ItemType SymbolicLink -Path "$scoopProfPath\user.js" -Value $userjsPath -Force
+  } else
+  {
+    Write-Host "There is no user.js file in .config/firefox"
+  }
+} else {
+  Write-Output "No valid firefox profile found."
 }
