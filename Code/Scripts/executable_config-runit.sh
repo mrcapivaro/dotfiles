1a3f8639372da79f7b24a2102bdd6ec5b9f2bd18
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-05 23:20:45 -0300

    Update Code/Scripts/config-runit.sh

--- Code/Scripts/executable_config-runit.sh
+++ Code/Scripts/executable_config-runit.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 set -e
 
-# Keymapper setup
+RUNIT_TARGET="/run/runit/service/"
+
+# keymapper begin
 KEYMAPPER_CONF_DIR="$HOME/.config/sv/keymapperd"
 KEYMAPPER_SV_DIR="/etc/runit/sv/keymapperd"
-KEYMAPPER_TARGET_DIR="/run/runit/service/keymapperd"
+
 if [ -d "$KEYMAPPER_SV_DIR" ]; then
-  echo "The keymapper sv folder already exists at $KEYMAPPER_SV_DIR."
+	echo "The keymapper sv folder already exists at $KEYMAPPER_SV_DIR."
 else
-  echo "Copying keymapper sv files from .config to $KEYMAPPER_SV_DIR."
-  sudo cp -r "$KEYMAPPER_CONF_DIR" "$KEYMAPPER_SV_DIR"
+	echo "Copying keymapper sv files from .config to $KEYMAPPER_SV_DIR."
+	sudo cp -r "$KEYMAPPER_CONF_DIR" "$KEYMAPPER_SV_DIR"
 fi
-echo "Symlinking the sv dir to runit."
-sudo ln -s "$KEYMAPPER_SV_DIR" "$KEYMAPPER_TARGET_DIR"
+
+if [ ! -d "$RUNIT_TARGET/keymapperd" ]; then
+	echo "Symlinking the sv dir to runit."
+	sudo ln -sf "$KEYMAPPER_SV_DIR" "$RUNIT_TARGET"
+fi
+# keymapper end
+
+# openntpd start
+if [ ! -d "$RUNIT_TARGET/openntpd" ]; then
+  echo "No openntpd sv found, creating symlink from /etc/runit to /run/runit."
+	sudo ln -sf /etc/runit/sv/openntpd /run/runit/service/
+	sudo sv start openntpd
+fi
+# openntpd end
