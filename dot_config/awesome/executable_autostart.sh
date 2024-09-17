f65dbfcffc938ce5c11e02b4e38df5f3af429c74
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-09-17 17:23:22 -0300

    Update .config/awesome/autostart.sh Update .config/awesome/rc.lua

--- dot_config/awesome/executable_autostart.sh
+++ dot_config/awesome/executable_autostart.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 run() {
-  if ! pgrep -f "$1$"; then
+  if ! pgrep -f "$1"; then
     "$@" &
   fi
 }
 
-# run dex --environment awesome --autostart --search-paths ~/.config/autostart
-run keymapper
-run picom
+run dex --environment Awesome --autostart --search-paths "/etc/xdg/autostart:$HOME/.config/autostart"
+# run picom
+# run pipewire
 run feh --bg-center "$HOME/.local/share/chezmoi/.other/mountains.png"
-run pipewire
+
 xrandr --output HDMI-0 --mode "1920x1080"
+setxkbmap "us, us(intl)" -option "grp:sclk_toggle"
--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -1,21 +1,22 @@
 --
 -- ~/.config/awesome/rc.lua
 --
--- Extensions to try later:
--- Lain[Widgets]: https://github.com/lcpz/lain
--- Vicious[Widgets]: https://vicious.readthedocs.io/en/latest/index.html
--- Bling[Func.]: https://blingcorp.github.io/bling/#/README
--- Rubato[Anim.]: https://github.com/andOrlando/rubato
---
--- Guides & Wiki:
--- Official Wiki: https://awesomewm.org/apidoc/index.html
--- Arch Wiki: https://wiki.archlinux.org/title/Awesome
--- Nice Guide: https://epsi-rns.github.io/desktop/2019/06/17/awesome-modularized-main.html
+
+-- === Extensions to try later ===
+-- Lain[Widgets]:     https://github.com/lcpz/lain
+-- Vicious[Widgets]:  https://vicious.readthedocs.io/en/latest/index.html
+-- Bling[Func.]:      https://blingcorp.github.io/bling/#/README
+-- Rubato[Anim.]:     https://github.com/andOrlando/rubato
+
+-- === Guides & Wiki ===
+-- Official Wiki:     https://awesomewm.org/apidoc/index.html
+-- Arch Wiki:         https://wiki.archlinux.org/title/Awesome
+-- Nice Guide:        https://epsi-rns.github.io/desktop/2019/06/17/awesome-modularized-main.html
 
 -- Call LuaRocks if possible
 pcall(require, "luarocks.loader")
 
--- Awesome default libraries
+-- Default libraries
 local gears = require("gears")
 local awful = require("awful")
 
@@ -29,23 +30,22 @@ beautiful.init(
     .. "/theme.lua"
 )
 
-require("main.error-handling")
-require("awful.autofocus")
-
 -- Layouts to be used. Order matters.
 awful.layout.layouts = {
-  awful.layout.suit.spiral,
+  awful.layout.suit.spiral.dwindle,
   awful.layout.suit.floating,
 }
 
-require("widgets.wibar")
 
 root.keys(require("binds.global"))
 root.buttons(require("binds.mouse"))
 
+require("awful.autofocus")
+require("widgets.wibar")
 require("main.rules")
 require("main.signals")
+require("main.error-handling")
 
 awful.spawn.with_shell(
-  gears.filesystem.get_configuration_dir() .. "autorun.sh"
+  gears.filesystem.get_configuration_dir() .. "autostart.sh"
 )
