06b0abd310ba8506980d1f28340325fe0405b20e
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-10-02 03:30:39 -0300

    Update .config/nvim/.stylua.toml Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/user-interface.lua Update .config/nvim/lua/settings.lua

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -1,6 +1,5 @@
 ---User Interace
 vim.g.FloatBorders = "single" -- { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
-require("statusline")
 
 ---Keymap Leaders
 vim.g.mapleader = " "
@@ -8,39 +7,39 @@ vim.g.maplocalleader = ","
 
 ---Vim Options
 local vim_opt = {
-  clipboard = "unnamedplus",
-  shiftwidth = 2,
-  tabstop = 2,
-  softtabstop = 2,
-  expandtab = true,
-  smartindent = true,
-  termguicolors = true,
-  number = true,
-  relativenumber = true,
-  signcolumn = "yes",
-  wrap = false,
-  list = true,
-  completeopt = "menu,menuone,noselect",
-  showmode = true,
-  cmdheight = 2,
-  colorcolumn = "80",
-  -- shellslash = false,
-  hlsearch = true,
-  incsearch = true,
-  autowrite = true,
-  swapfile = false,
-  backup = false,
-  ignorecase = true,
-  spelllang = { "en_US.UTF-8" },
-  scrolloff = 16,
-  sidescrolloff = 8,
-  updatetime = 50,
-  virtualedit = "block",
-  conceallevel = 2,
-  concealcursor = "nc",
-  cursorline = true,
+    clipboard = "unnamedplus",
+    shiftwidth = 4,
+    tabstop = 4,
+    softtabstop = 4,
+    expandtab = true,
+    smartindent = true,
+    termguicolors = true,
+    number = true,
+    relativenumber = true,
+    signcolumn = "yes",
+    wrap = false,
+    list = true,
+    completeopt = "menu,menuone,noselect",
+    showmode = true,
+    cmdheight = 2,
+    colorcolumn = "80",
+    -- shellslash = false,
+    hlsearch = true,
+    incsearch = true,
+    autowrite = true,
+    swapfile = false,
+    backup = false,
+    ignorecase = true,
+    spelllang = { "en_US.UTF-8" },
+    scrolloff = 16,
+    sidescrolloff = 8,
+    updatetime = 50,
+    virtualedit = "block",
+    conceallevel = 2,
+    concealcursor = "nc",
+    cursorline = true,
 }
 
 for k, v in pairs(vim_opt) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
