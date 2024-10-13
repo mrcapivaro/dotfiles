fef1be1218d4d77ebe561d0e023f8f41965e2e03
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-10-13 04:11:26 -0300

    Update .config/nvimpager/init.lua

--- dot_config/nvimpager/init.lua
+++ dot_config/nvimpager/init.lua
@@ -1,1 +1,10 @@
-vim.cmd([[colorscheme habamax]])
+--
+-- ~/.config/nvimpager/init.lua
+--
+
+
+-- [ Colorscheme ] --
+
+local colorscheme = "catppuccin"
+vim.opt.rtp:prepend("~/.local/share/nvim/lazy/" .. colorscheme)
+vim.cmd("colorscheme " .. colorscheme)
