8c328c111d48a1becdbb8dd70e4fe8bb48581aee
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-11-29 00:48:53 -0300

    Update .config/nvimpager/init.lua

--- dot_config/nvimpager/init.lua
+++ dot_config/nvimpager/init.lua
@@ -1,10 +1,6 @@
---
 -- ~/.config/nvimpager/init.lua
---
 
-
--- [ Colorscheme ] --
-
-local colorscheme = "catppuccin"
+---Colorscheme
+local colorscheme = "gruvbox"
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/" .. colorscheme)
-vim.cmd("colorscheme " .. colorscheme)
+vim.cmd.colorscheme(colorscheme)
