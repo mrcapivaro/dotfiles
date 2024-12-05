9ea17f662fd074708d79db29a09051df22b3f35b
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-12-05 15:35:42 -0300

    Remove .config/awesome/icons/void-logo-notext.svg Remove .config/awesome/lua/cyclefocus/init.lua Remove .config/awesome/lua/helpers/init.lua Remove .config/awesome/lua/poppin/init.lua Remove .config/awesome/lua/poppin/session.lua Update .config/rofi/colors/gruvbox.rasi Update .config/rofi/scripts/powermenu.sh Add .config/rofi/scripts/symbols.sh Add .config/rofi/scripts/symbols.txt Update .config/rofi/themes/launcher.rasi Update .config/rofi/themes/qalc.rasi

--- dot_config/rofi/scripts/executable_powermenu.sh
+++ dot_config/rofi/scripts/executable_powermenu.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rofi -mode ??? \
-    -show drun
+if [ $# -eq 0 ]; then
+    rofi -mode powermenu \
+         -show "powermenu:powermenu.sh" \
+         -theme "./themes/launcher.rasi"
+fi
