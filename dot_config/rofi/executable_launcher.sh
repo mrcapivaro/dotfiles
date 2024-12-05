7b7cb70a1863fdeaeee8a46208edc2322f3edbcf
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-12-05 15:37:32 -0300

    update

--- dot_config/rofi/executable_launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rofi -mode drun \
-    -show drun \
-    -theme "./themes/launcher.rasi"
--- dot_config/rofi/executable_powermenu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rofi -mode ??? \
-    -show drun
