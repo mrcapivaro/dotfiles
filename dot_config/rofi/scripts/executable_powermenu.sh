a5d0a1e16dafc5d1143d5c6dde9fda119cf5a9a0
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-12-08 00:47:08 -0300

    update

--- dot_config/rofi/scripts/executable_powermenu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [ $# -eq 0 ]; then
-    rofi -mode powermenu \
-         -show "powermenu:powermenu.sh" \
-         -theme "./themes/launcher.rasi"
-fi
--- dot_config/rofi/scripts/executable_window-switcher.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rofi -mode window \
-    -show window \
-    -theme "./themes/window-switcher.rasi" \
-    -selected-row 1 \
-    -kb-element-next "Alt_L+Tab" \
-    -kb-accept-entry "!Alt_L"
