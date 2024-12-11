f914a1bfeefc38e892f15b93b59b777bb092f140
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-12-11 02:39:00 -0300

    Update .config/firefox/config-firefox.sh

--- dot_config/firefox/executable_config-firefox.sh
+++ dot_config/firefox/executable_config-firefox.sh
@@ -20,4 +20,4 @@ curl \
     -fsSl https://raw.githubusercontent.com/tridactyl/native_messenger/master/installers/install.sh \
     -o /tmp/trinativeinstall.sh && sh /tmp/trinativeinstall.sh 1.24.1
 
-echo "[*] Done"
+echo "[*] Done."
