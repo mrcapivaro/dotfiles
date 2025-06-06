7a1b4eea89b91f233171d2dece1fb01f98ddd2b2
mrcapivaro <guilhermepachecobatista@gmail.com>
2025-06-06 15:53:18 -0300

    Update .config/alacritty/gruvbox_dark.toml Update .config/mimeapps.list Update .config/tmux/tmux.conf Remove .config/zellij/config.kdl Remove .config/zellij/layouts/default.kdl Remove .config/zellij/layouts/ftil.kdl Remove .config/zellij/layouts/rtil.kdl Remove .config/zellij/update.sh

--- dot_config/zellij/update.sh
+++
@@ -1,8 +0,0 @@
-zellij_pane_name_update() {
-    [[ -z $ZELLIJ ]] && return
-    local current_dir="${PWD/${HOME}/~}"
-    command nohup zellij action rename-pane $current_dir >/dev/null 2>&1
-}
-
-zellij_pane_name_update
-chpwd_functions+=(zellij_pane_name_update)
